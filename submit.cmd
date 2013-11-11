cbc892e84c458590951c8dab79539d6b1cbdff1d
Marco Lombardi <marco.lombardi@tum.de>
2013-11-11 12:55:49 +0100

    an Maier angepasst

--- submit.cmd
+++ submit.cmd
@@ -4,20 +4,16 @@
 ##
 #
 #@ job_type = parallel
-#@ class = fat
-#@ node = 4
-###@ island_count= not needed for
-#### class general
-##@ total_tasks= 156
-## other example
+#@ class = test
+#@ node = 1
 #@ tasks_per_node = 1
 #@ wall_clock_limit = 1:20:30
 ##                    1 h 20 min 30 secs
-#@ job_name = mytest
+#@ job_name = mmult
 #@ network.MPI = sn_all,not_shared,us
 #@ initialdir = $(home)/
-#@ output = job$(jobid).out
-#@ error = job$(jobid).err
+#@ output = mmult$(jobid).out
+#@ error = mmult$(jobid).err
 #@ notification=always
 #@ notify_user=youremailaddress@yomama.com
 #@ queue
